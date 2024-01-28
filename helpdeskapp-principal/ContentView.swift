9cd337ed7120fa8fc4e651c2234c055bbf99c451
Arlid Henao Rueda <arlidhenaorueda@gmail.com>
2024-01-27 21:36:58 -0500

    Initial Commit

---
+++ helpdeskapp-principal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helpdeskapp-principal
+//
+//  Created by Arlid Henao Rueda on 27/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helpdeskapp-principal/helpdeskapp_principalApp.swift
@@ -0,0 +1,17 @@
+//
+//  helpdeskapp_principalApp.swift
+//  helpdeskapp-principal
+//
+//  Created by Arlid Henao Rueda on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct helpdeskapp_principalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
